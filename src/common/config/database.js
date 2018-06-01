624a2cb17eb1edc3b935558bfeb19482a935b5a7
dingmike <mikezhangding@hotmail.com>
2018-06-01 22:43:11 +0800

    init

--- src/common/config/database.js
+++ src/common/config/database.js
@@ -5,9 +5,9 @@ module.exports = {
   database: 'nideshop',
   prefix: 'nideshop_', // 数据表前缀，如果一个数据库里有多个项目，那项目之间的数据表可以通过前缀来区分
   encoding: 'utf8mb4',
-  host: '47.106.85.97',
+  host: '',
   port: '3306',
-  user: 'dingdingnewscnn',
-  password: 'mikezhidaoZdj!@#123', //1qazqaz1
+  user: '',
+  password: '',
   dateStrings: true
 };
